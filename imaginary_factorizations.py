7e4afaf175ed296c51719ebd9bf8e680c87ff5a7
GrumpyC <111717366+Grumpy-C@users.noreply.github.com>
2026-01-21 14:20:34 +1300

    Update and rename mitfactorization.py to imaginary_factorizations.py

--- imaginary_factorizations.py
+++ imaginary_factorizations.py
@@ -1,9 +1,11 @@
+# original file name: mitfactorization.py
 import math
 
-inp = 7152154301
+inp = 7152154301 # example value
 min = math.floor(math.sqrt(inp))
 
 for i in range(min, 0, -1):
     b = math.sqrt(inp - i**2)
     if abs(b - round(b)) < 0.0000001: # avoid floating point problems
+
         print(i, b)
